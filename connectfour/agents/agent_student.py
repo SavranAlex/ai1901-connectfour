ece523e8d57f994a0f6af6ae7abe3f8c818aa15b
Alex <Savranalex@gmail.com>
2019-04-25 15:00:21 +1000

    added defs and vars descriptions to use from board.py

--- connectfour/agents/agent_student.py
+++ connectfour/agents/agent_student.py
@@ -30,7 +30,7 @@ class StudentAgent(RandomAgent):
 
     def dfMiniMax(self, board, depth):
         # Goal return column with maximized scores of all possible next states
-        
+
         if depth == self.MaxDepth:
             return self.evaluateBoardState(board)
 
@@ -43,11 +43,11 @@ class StudentAgent(RandomAgent):
                 next_state = board.next_state(self.id % 2 + 1, move[1])
             else:
                 next_state = board.next_state(self.id, move[1])
-                
+
             moves.append( move )
             vals.append( self.dfMiniMax(next_state, depth + 1) )
 
-        
+
         if depth % 2 == 1:
             bestVal = min(vals)
         else:
@@ -57,36 +57,36 @@ class StudentAgent(RandomAgent):
 
     def evaluateBoardState(self, board):
         """
-        Your evaluation function should look at the current state and return a score for it. 
-        As an example, the random agent provided works as follows:
-            If the opponent has won this game, return -1.
-            If we have won the game, return 1.
-            If neither of the players has won, return a random number.
-        """
-        
-        """
-        These are the variables and functions for board objects which may be helpful when creating your Agent.
-        Look into board.py for more information/descriptions of each, or to look for any other definitions which may help you.
+        possible vars and functions from board.def
 
-        Board Variables:
-            board.width 
-            board.height
-            board.last_move
-            board.num_to_connect
-            board.winning_zones
-            board.score_array 
-            board.current_player_score
+        functions:
+            def get_cell_value(self, row, col): returns 1 if cell has player1 token, 2 if player's2 token, 0 if empty
+            def try_move(self, move): returns raw num where token will be located, if move is impossible returns -1
+            def valid_move(self, row, col): returns true if row and column exists and Empty
+            def valid_moves(self): iterator of valid moves for current_board
+            def terminal(self): true if game is finished, false otherwise
+            def legal_moves(self): returns valid moves for the next player
+            def next_state(self, turn, col): returns the next board after the proposed move
+            def _empty_board(self, height, width): returns empty board
+            def winner(self): returns 1 or 2 if player1 or player2 win otherwise 0
+            def _check_rows(self): returns the number of connected tokens of the same color or 0 if no tokens connected for rows
+            def _check_columns(self): returns the number of connected tokens of the same color or 0 if no tokens connected for columns
+            def _check_diagonals(self): returns the number of connected tokens of the same color or 0 if no tokens connected for _check_diagonals
 
-        Board Functions:
-            get_cell_value(row, col)
-            try_move(col)
-            valid_move(row, col)
-            valid_moves()
-            terminal(self)
-            legal_moves()
-            next_state(turn)
-            winner()
+        vars:
+            board=None
+            height=None - num
+            width=None - num
+            last_move=[None, None] - [raw, col] of last move
+            num_to_connect=4
+            winning_zones - as far as I understand it is tuple of valid moves [raw, col] where putting a token makes sense
+            score_array - scores of players [player1, player2], not sure what scores mean here
+            current_player_score - not used anywhere, have no idea what is it's purpose
         """
-				
-        return random.uniform(0, 1)
 
+        current_board = board
+        next_board = NULL
+
+
+
+        #return random.uniform(0, 1)
